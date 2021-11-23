642d27526e25c9bc000f30bb611d99df4d15b014
Kirill <temerev.ks@yandex.ru>
2021-11-24 01:05:44 +0300

    Initialize flask app
    Create index.html
    Add text content
    Create base structure inside index.html with classes and comments
    
    Create style.css
    select base classes

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
+from flask_bootstrap import Bootstrap
+from datetime import date
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template(
+        'index.html',
+        current_date=date.today().year
+    )
+
+
+@app.route('/bootstrap')
+def bootstrap_page():
+    return render_template('base.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
