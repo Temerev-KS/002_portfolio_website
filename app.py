29d420e7b5b75496f551a51bf9f19d61196cf64a
Kirill <temerev.ks@yandex.ru>
2021-12-10 22:34:04 +0300

    Final stages of the project
    Delete of bootstrap version of development

--- app.py
+++ app.py
@@ -12,13 +12,6 @@ def index():
     )
 
 
-@app.route('/bootstrap')
-def bootstrap_page():
-    return render_template(
-        'index-bootstrap.html',
-        current_date=date.today().year
-    )
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
